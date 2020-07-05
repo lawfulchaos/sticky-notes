6e96a58d041e29e251abb61ec791ae16054b4b06
LawfulChaos <kerbalspacema@gmail.com>
2020-07-05 16:01:21 +0000

    Add missing changes

--- notepad/urls.py
+++ notepad/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from register import views as reg_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('notes.urls')),
+    path("register/", reg_views.register, name="register"),
+    path('', include("notes.urls")),
+    path('', include("django.contrib.auth.urls")),
 ]
--- notes/views.py
+++ notes/views.py
@@ -3,6 +3,7 @@ from .models import Note
 from django.shortcuts import render, get_object_or_404
 from .forms import NoteForm
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 
 from random import choice
 colors = ["red_note", "green_note", "blue_note", "purple_note", "yellow_note"]
@@ -11,13 +12,22 @@ def note_list(request):
     notes = Note.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'notes/note_list.html', {'notes': notes})
 
+def note_by_author(request, user):
+    notes = Note.objects.filter(published_date__lte=timezone.now(), author__username=user).order_by('published_date')
+    return render(request, 'notes/note_list.html', {'notes': notes, 'author_name': user})
+
+
+def note_secret(request):
+    return render(request, 'notes/note_secret.html', {})
+
+@login_required(login_url='/login')
 def note_new(request):
     if request.method == "POST":
         form = NoteForm(request.POST)
         if form.is_valid():
             note = form.save(commit=False)
             note.color = choice(colors)
-            #note.author = User.objects.all()[0]
+            note.author = request.user
             note.published_date = timezone.now()
             note.save()
             return redirect('note_details', pk=note.pk)
@@ -28,4 +38,5 @@ def note_new(request):
 def note_details(request, pk):
    note = get_object_or_404(Note, pk=pk)
    return render(request, 'notes/note_details.html', {'note': note})
+
 # Create your views here.
